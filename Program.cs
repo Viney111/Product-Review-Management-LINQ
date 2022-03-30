8cb1095be9d5f3458be697626a356c49690147af
Viney <vineykhaneja999@gmail.com>
2022-03-30 21:22:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product_Review_Management_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductReviewManager manager = new ProductReviewManager();
+            List<ProductReview> productReviewFilledList = new List<ProductReview>();
+            productReviewFilledList = manager.AddingValuesInProductReviewList(productReviewFilledList);
+            foreach(ProductReview productReview in productReviewFilledList)
+            {
+                Console.WriteLine(productReview.ToString());
+            }
+        }
+    }
+}
